f7636fdb58af9da9634659cb99422b76e80cba9e
mohammad-and-nesma <mohammad-and-nesma@users.noreply.github.com>
2025-05-18 08:29:21 +0000

    deploy: a20c9b01041e45d9442a3bf1134a4dac1e438fd6

--- _astro/index.astro_astro_type_script_index_0_lang.16WaFWT4.js
+++
@@ -1,1 +0,0 @@
-import{i as n,a as t}from"./main.BohvJ9Vz.js";n();const o=document.getElementById("countdown");t(o);
--- _astro/index.astro_astro_type_script_index_0_lang.D1Z12sTn.js
+++
@@ -1,1 +0,0 @@
-import{i as t,a as n}from"./main.BohvJ9Vz.js";t();const o=document.getElementById("countdown");n(o,!0);
